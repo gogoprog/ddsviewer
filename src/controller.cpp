890fd4258bd07578d4d8400f8a51e4de8bd691f6
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-04-12 09:49:45 +0200

    Drag the sprite

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,7 +1,28 @@
 #include "controller.h"
 
+#include <iostream>
+#include <SFML/System/Vector2.hpp>
+
+struct Controller::Pimpl
+{
+    sf::Vector2f mouseStart;
+    sf::Vector2f spriteStart;
+    bool movingSprite = false;
+};
+
+Controller::~Controller() = default;
+
+Controller::Controller(sf::RenderWindow & _window, sf::Sprite & _sprite)
+    :
+    window(_window),
+    sprite(_sprite),
+    _pimpl(new Pimpl())
+{
+}
+
 void Controller::process()
 {
+    auto & pimpl = *_pimpl;
     sf::Event event;
 
     while (window.pollEvent(event))
@@ -39,6 +60,31 @@ void Controller::process()
                 else
                 {
                     sprite.setScale(sprite.getScale() / 1.1f);
+                }
+            }
+            break;
+
+            case sf::Event::MouseButtonPressed:
+            {
+                pimpl.mouseStart = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+                pimpl.spriteStart = sprite.getPosition();
+                pimpl.movingSprite = true;
+            }
+            break;
+
+            case sf::Event::MouseButtonReleased:
+            {
+                pimpl.movingSprite = false;
+            }
+            break;
+
+            case sf::Event::MouseMoved:
+            {
+                if(pimpl.movingSprite)
+                {
+                    auto delta = sf::Vector2f(event.mouseMove.x, event.mouseMove.y) - pimpl.mouseStart;
+
+                    sprite.setPosition(pimpl.spriteStart + delta);
                 }
             }
             break;
--- src/controller.h
+++ src/controller.h
@@ -1,20 +1,20 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <memory>
 
 class Controller
 {
 public:
-    Controller(sf::RenderWindow & _window, sf::Sprite & _sprite)
-        :
-        window(_window),
-        sprite(_sprite)
-    {
-    }
+    Controller(sf::RenderWindow & _window, sf::Sprite & _sprite);
+    ~Controller();
 
     void process();
 
 private:
     sf::Sprite & sprite;
     sf::RenderWindow & window;
+
+    struct Pimpl;
+    std::unique_ptr<Pimpl> _pimpl;
 };
